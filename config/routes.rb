4f7b59df4a73353a5a4b9a1fd38bb6189b9c852f
Emmet Susslin <emmet@Emmets-MacBook-Pro.local>
2017-06-13 19:08:43 -0700

    lol

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   
 #https://calm-castle-11120.herokuapp.com/
 
+# git push heroku master
+# If you're not developing on master deploy with:
+
+# git push heroku your_branch_name:master
+
 
   post 'proposals/url', to: 'proposals#url'
 
